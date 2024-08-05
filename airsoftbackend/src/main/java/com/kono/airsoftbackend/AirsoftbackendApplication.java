02206dec7fd9149c4025eadf23109b9347d4b3b4
konosaiko <xkonotv@gmail.com>
2024-08-05 15:19:20 +0200

    Initial commit - Spring boot

---
+++ airsoftbackend/src/main/java/com/kono/airsoftbackend/AirsoftbackendApplication.java
@@ -0,0 +1,13 @@
+package com.kono.airsoftbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirsoftbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirsoftbackendApplication.class, args);
+	}
+
+}
---
+++ airsoftbackend/src/test/java/com/kono/airsoftbackend/AirsoftbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kono.airsoftbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirsoftbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
