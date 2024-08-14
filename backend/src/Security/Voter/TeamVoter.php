29b94fedaadeb04701306e3a0c7adb174ece68c6
konosaiko <xkonotv@gmail.com>
2024-08-14 23:20:47 +0200

    Ajout d'un voter pour l'entité Team

---
+++ backend/src/Security/Voter/TeamVoter.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\Team;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+class TeamVoter extends Voter
+{
+    protected function supports(string $attribute, mixed $subject): bool
+    {
+        return in_array($attribute, ['TEAM_EDIT', 'TEAM_DELETE'])
+            && $subject instanceof Team;
+    }
+
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+        if (!$user instanceof User) {
+            return false;
+        }
+
+        /** @var Team $team */
+        $team = $subject;
+
+        switch ($attribute) {
+            case 'TEAM_EDIT':
+            case 'TEAM_DELETE':
+                return $this->canEditOrDelete($team, $user);
+        }
+
+        return false;
+    }
+
+    private function canEditOrDelete(Team $team, User $user): bool
+    {
+        return $user === $team->getCreator();
+    }
+}
