4082b7cd48d05581ab031935b0685796c332f2d5
konosaiko <xkonotv@gmail.com>
2024-08-16 13:44:16 +0200

    Ajout d'un service pour TeamMember

---
+++ backend/src/Service/TeamMemberService.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Team;
+use App\Entity\TeamMember;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+
+class TeamMemberService
+{
+    private $entityManager;
+    private $notificationService;
+
+    public function __construct(EntityManagerInterface $entityManager, NotificationService $notificationService)
+    {
+        $this->entityManager = $entityManager;
+        $this->notificationService = $notificationService;
+    }
+
+    public function createJoinRequest(Team $team, User $user): TeamMember
+    {
+        $existingRequest = $this->entityManager->getRepository(TeamMember::class)->findOneBy([
+            'team' => $team,
+            'user' => $user
+        ]);
+
+        if ($existingRequest) {
+            throw new \Exception('A request for this team already exists.');
+        }
+
+        $teamMember = new TeamMember();
+        $teamMember->setTeam($team);
+        $teamMember->setUser($user);
+
+        $this->entityManager->persist($teamMember);
+        $this->entityManager->flush();
+
+        $this->notificationService->createNotification(
+            $team->getCreator(),
+            "New join request for team {$team->getName()} from user {$user->getUsername()}"
+        );
+
+        return $teamMember;
+    }
+
+    public function approveJoinRequest(TeamMember $teamMember): void
+    {
+        $teamMember->approve();
+        $this->entityManager->flush();
+
+        $this->notificationService->createNotification(
+            $teamMember->getUser(),
+            "Your request to join team {$teamMember->getTeam()->getName()} has been approved."
+        );
+    }
+
+    public function rejectJoinRequest(TeamMember $teamMember): void
+    {
+        $teamMember->reject();
+        $this->entityManager->flush();
+
+        $this->notificationService->createNotification(
+            $teamMember->getUser(),
+            "Your request to join team {$teamMember->getTeam()->getName()} has been rejected."
+        );
+    }
+
+    public function getPendingRequests(Team $team): array
+    {
+        return $this->entityManager->getRepository(TeamMember::class)->findBy([
+            'team' => $team,
+            'status' => 'pending'
+        ]);
+    }
+
+    public function promoteToOfficer(TeamMember $teamMember): void
+    {
+        
+    }
+}
