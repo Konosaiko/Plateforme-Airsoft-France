e7a105547b129a7ec6a5d3e2955b411b10f5e7f3
konosaiko <xkonotv@gmail.com>
2024-08-17 03:42:03 +0200

    Création d'un form AssociationType

---
+++ backend/src/Form/AssociationType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Association;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AssociationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Association Name',
+                'attr' => ['placeholder' => 'Enter the association name'],
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+                'attr' => ['placeholder' => 'Describe the association'],
+            ])
+            ->add('address', TextType::class, [
+                'label' => 'Address',
+                'attr' => ['placeholder' => 'Enter the association address'],
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => ['placeholder' => 'Enter the contact email'],
+            ])
+            ->add('phone', TelType::class, [
+                'label' => 'Phone',
+                'attr' => ['placeholder' => 'Enter the contact phone number'],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Association::class,
+        ]);
+    }
+}
