e0dcfb04abc21dc26b191bbf49038a41454a92a1
konosaiko <xkonotv@gmail.com>
2024-08-17 03:43:07 +0200

    Création form InviteMemberType

---
+++ backend/src/Form/InviteMemberType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use App\Entity\AssociationMember;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class InviteMemberType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('user', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'username',
+                'label' => 'Select User',
+            ])
+            ->add('role', ChoiceType::class, [
+                'choices' => [
+                    'Admin' => 'OWNER',
+                    'Member' => 'MEMBER',
+                ],
+                'label' => 'Role',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
